858ee76ffcb850193b9053084d113be77528eaf7
Jeongseok Lee <jeongseok@meta.com>
2025-11-29 15:44:29 -0800

    Make build.sh cross-compilation check more robust

--- recipe/build.sh
+++ recipe/build.sh
@@ -1,11 +1,24 @@
 #!/bin/bash
 set -ex
 
+echo "CONDA_BUILD_CROSS_COMPILATION=${CONDA_BUILD_CROSS_COMPILATION:-}"
+echo "target_platform=${target_platform:-}"
+echo "build_platform=${build_platform:-}"
+echo "uname -m=$(uname -m)"
+
+IS_CROSS=0
 if [[ "${CONDA_BUILD_CROSS_COMPILATION:-}" == "1" ]]; then
+  IS_CROSS=1
+elif [[ "$target_platform" == "osx-arm64" && "$(uname -m)" == "x86_64" ]]; then
+  IS_CROSS=1
+fi
+
+if [[ "$IS_CROSS" == "1" ]]; then
   echo "Cross-compiling detected."
   # Use build python to drive installation
   # We point pip to install into the target prefix
-  $BUILD_PREFIX/bin/python -m pip install . -vv --no-deps --no-build-isolation --prefix "$PREFIX"
+  # We use 'python' from PATH which should be the build python (from requirements: build)
+  python -m pip install . -vv --no-deps --no-build-isolation --prefix "$PREFIX"
 else
   # Native build
   $PYTHON -m pip install . -vv --no-deps --no-build-isolation
